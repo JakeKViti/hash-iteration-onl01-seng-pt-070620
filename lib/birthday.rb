c529aaf61028d118895799c8631cfa76f5273255
Jacob Viti <thevitij64@gmail.com>
2020-07-12 03:27:40 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,7 +14,7 @@ def age_appropriate_birthday(birthday_kids)
     birthday_kids.each do |kids_name, age|
       if age > 13
     puts "You are too old for this."
-  elsif age < 13
+    elsif age < 13
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
   end
