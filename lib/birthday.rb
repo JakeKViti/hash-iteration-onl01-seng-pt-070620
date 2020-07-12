ea1d6189d6c96168d3c2488da5d736f83b86771e
Jacob Viti <thevitij64@gmail.com>
2020-07-12 03:23:49 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,8 +4,10 @@
   "Amanda" => 27
 }
 
-  def happy_birthday(birthday_kids)
+def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
+    if age < 13
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
   end
 end
