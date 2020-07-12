ec173d38a8eff64b436e5706b17d6614e7531c22
Jacob Viti <thevitij64@gmail.com>
2020-07-12 03:26:09 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,7 @@ end
 def age_appropriate_birthday(birthday_kids)
     birthday_kids.each do |kids_name, age|
       if age < 13
-      print "Happy Birthday #{kids_name}! You are now #{age} years old!"
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
     end
   end
